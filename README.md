392adb7667857c11dc81ab60bda565a4f3808d6f
Benjamin Davis <45952064+bmdavis419@users.noreply.github.com>
2022-11-13 16:04:27 +0000

    created base of project

--- README.md
+++
@@ -1,2 +0,0 @@
-# weights-tracker
-A simple powerlifting/bodybuilding weights tracker built in React using Firebase as a backend.
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-center text-2xl mt-3">hello world</div>;
+}
+
+export default App;
